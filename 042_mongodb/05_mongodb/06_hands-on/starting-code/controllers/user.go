24c12e5a48f21783f84c637be7179934b46132ea
skhut <sk2gcp@gmail.com>
2020-05-30 20:25:36 +0530

    adding 06_hands on code with solution

---
+++ 042_mongodb/05_mongodb/06_hands-on/starting-code/controllers/user.go
@@ -0,0 +1,93 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/skhut/go-practice-web-dev-db/042_mongodb/06_hands-on/starting-code/models"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type UserController struct {
+	sessionMap *map[string]User
+}
+
+func NewUserController(s *map[string]User) *UserController {
+	return &UserController{s}
+}
+
+func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	// Grab id
+	id := p.ByName("id")
+
+	// Verify id is ObjectId hex representation, otherwise return status not found
+	if !bson.IsObjectIdHex(id) {
+		w.WriteHeader(http.StatusNotFound) // 404
+		return
+	}
+
+	// ObjectIdHex returns an ObjectId from the provided hex representation.
+	oid := bson.ObjectIdHex(id)
+
+	// composite literal
+	u := models.User{}
+
+	u, ok := uc.sessionMap[string(oid)]
+
+	if !ok {
+		w.WriteHeader(404)
+		return
+	}
+
+	// Marshal provided interface into JSON structure
+	uj, _ := json.Marshal(u)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK) // 200
+	fmt.Fprintf(w, "%s\n", uj)
+}
+
+func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	u := models.User{}
+
+	json.NewDecoder(r.Body).Decode(&u)
+
+	// create bson ID
+	u.Id = bson.NewObjectId()
+
+	// store the user in mongodb
+	uc.sessionMap[string(u.Id)] = u
+
+	uj, _ := json.Marshal(u)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated) // 201
+	fmt.Fprintf(w, "%s\n", uj)
+}
+
+func (uc UserController) DeleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
+
+	if !bson.IsObjectIdHex(id) {
+		w.WriteHeader(404)
+		return
+	}
+
+	oid := bson.ObjectIdHex(id)
+
+	// composite literal
+	u := models.User{}
+
+	u, ok := uc.sessionMap[string(oid)]
+
+	if !ok {
+		w.WriteHeader(404)
+		return
+	}
+	delete(uc.sessionMap, oid)
+
+	w.WriteHeader(http.StatusOK) // 200
+	fmt.Fprint(w, "Deleted user", oid, "\n")
+}
---
+++ 042_mongodb/05_mongodb/06_hands-on/starting-code/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/skhut/go-practice-web-dev-db/042_mongodb/06_hands-on/starting-code/controllers"
+)
+
+func main() {
+	r := httprouter.New()
+	// Get a UserController instance
+	uc := controllers.NewUserController(getSession())
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe("localhost:8080", r)
+}
+
+func getSession() *map[string]User {
+	return make(map[string]User)
+}
